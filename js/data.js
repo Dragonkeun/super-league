7d5e86d6da0e9763ccc5b6defc02b11785e46316
Dragonkeun <dragonkeun7@gmail.com>
2023-03-10 20:57:25 +0900

    슈퍼리그 토이프로젝트 시작, data 세팅 테스트 완료

---
+++ js/data.js
@@ -0,0 +1,34 @@
+// team
+// id
+// 영어팀명
+// 한국어팀명
+// 주장이름 카림 벤제마
+// 주장사진
+// 감독이름  
+// 감독사진
+// 로고사진
+// 유니폼사진
+// 소속리그
+// 순위
+
+// player 
+// id(team의 id와 동일하게, 레알 마드리드의 id는 0, 레알 마드리드의 선수도 0으로)
+// 이름
+// 포지션
+// 나이
+// 국적
+// 스탯
+
+export const team = [
+  {
+    "id" : 0,
+    "engname" : "Real Madrid",
+    "korname" : "레알 마드리드",
+    "captainname" : "카림 벤제마",
+    "captainimg" : "../img/karimbenzema.webp",
+    "coachname" : "카를로 안첼로티",
+    "coachimg" : "",
+
+  }
+
+]
---
+++ js/index.js
@@ -0,0 +1,6 @@
+import {team} from "./data.js";
+
+console.log(team[0].korname);
+console.log(team.lengtth);
+
+document.querySelector(".img").src = team[0].captainimg
